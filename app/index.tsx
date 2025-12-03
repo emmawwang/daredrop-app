cc67e3b0d5469f2562070b6948d4120190e979cd
Emma Wang <emmawwang@gmail.com>
2025-12-02 16:57:11 -0800

    increasing padding on container

--- app/index.tsx
+++ app/index.tsx
@@ -120,7 +120,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    paddingTop: 20,
+    paddingTop: 50,
     alignItems: "center",
   },
   header: {
